1b08b5cd180eacaac1c6325d99264891423cacd9
Daniel Musser <danielmusser@daniels-imac.local>
2017-02-04 17:23:37 -0700

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Daniel Musser on 2/4/17.
+//  Copyright © 2017 Daniel Musser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
