455a13484f4cbc665685ffcfe4c8922e3f8858e7
Daniel Musser <danielmusser@daniels-imac.local>
2017-02-05 12:28:26 -0700

    Finished get functions.

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -18,12 +18,29 @@ class ViewController: UIViewController {
     
     func getPositiveAnswer () -> String {
         var chosenAnswer = ""
-        let randomchoice = arc4random_uniform(11)
+        let randomchoice = arc4random_uniform(10)
         
         switch randomchoice {
         case 0:
             chosenAnswer = "It is certain"
-            
+        case 1:
+            chosenAnswer = "It is decidedly so"
+        case 2:
+            chosenAnswer = "Without a doubt"
+        case 3:
+            chosenAnswer = "Yes, definitely"
+        case 4:
+            chosenAnswer = "You may rely on it"
+        case 5:
+            chosenAnswer = "As I see it, yes"
+        case 6:
+            chosenAnswer = "Most likely"
+        case 7:
+            chosenAnswer = "Outlook good"
+        case 8:
+            chosenAnswer = "Yes"
+        case 9:
+            chosenAnswer = "Signs point to yes"
         default:
             chosenAnswer = "It's not the spoon that moves, but you..."
         }
@@ -38,8 +55,15 @@ class ViewController: UIViewController {
         
         switch randomchoice {
         case 0:
-            chosenAnswer = "It is certain"
-            
+            chosenAnswer = "Reply hazy try again"
+        case 1:
+            chosenAnswer = "Ask again later"
+        case 2:
+            chosenAnswer = "Better not tell you now"
+        case 3:
+            chosenAnswer = "Cannot predict now"
+        case 4:
+            chosenAnswer = "Concentrate and ask again"
         default:
             chosenAnswer = "It's not the spoon that moves, but you..."
         }
@@ -49,12 +73,19 @@ class ViewController: UIViewController {
     
     func getNegativeAnswer () -> String {
         var chosenAnswer = ""
-        let randomchoice = arc4random_uniform(11)
+        let randomchoice = arc4random_uniform(5)
         
         switch randomchoice {
         case 0:
-            chosenAnswer = "It is certain"
-            
+            chosenAnswer = "Don't count on it"
+        case 1:
+            chosenAnswer = "My reply is no"
+        case 2:
+            chosenAnswer = "My sources say no"
+        case 3:
+            chosenAnswer = "Outlook not so good"
+        case 4:
+            chosenAnswer = "Very doubtful"            
         default:
             chosenAnswer = "It's not the spoon that moves, but you..."
         }
